921f77aaeee03a768ad555bd675cf519a00be4b0
trainzkid <trainzkid@gmail.com>
2023-09-28 01:08:25 -0500

    Fixed rng to exactly scale with the bitset size (I think?) and disabled other broken stuff for now

--- src/maze.cpp
+++ src/maze.cpp
@@ -2,22 +2,29 @@
 #include <iostream>
 #include <random>
 #include <bitset>
+#include <cmath>
 
 int main() {
-	enum wall : unsigned short {north,south,east,west,};
-		   //northeast,northwest,southeast,southwest};
-	std::vector<std::vector<std::bitset<4>>> maze(10,std::vector<std::bitset<4>>(10,0));		// bitset for each cardinal direction: 0001=N,0010=E,0100=S,1000=W,etc
-	std::vector<std::vector<bool>> visitedCells(10,std::vector<bool>(10,false));
+	const unsigned short DIRECTIONS=4;
+	const int MAZE_SIZE=10;
+	enum wall : unsigned short {north,south,east,west,
+		   northeast,northwest,southeast,southwest,
+		   north_northeast,east_northeast,north_northwest,west_northwest,south_southeast,east_southeast,south_southwest,west_southwest};
+	std::vector<std::vector<std::bitset<DIRECTIONS>>> mazeWalls(MAZE_SIZE,std::vector<std::bitset<DIRECTIONS>>(MAZE_SIZE,0));
+	std::vector<std::vector<bool>> visitedCells(MAZE_SIZE,std::vector<bool>(MAZE_SIZE,false));
 
-	//std::random_device r;
-	//std::mt19937 rng(r());
-	//std::uniform_int_distribution<std::mt19937::result_type> distribution(0,1);
+	std::random_device r;
+	std::mt19937 rng(r());
+	//rng.seed(1024);
+	
+	std::uniform_int_distribution<std::mt19937::result_type> distribution(0,std::pow(2,mazeWalls.at(0).at(0).size())-1);		// generate num from 0 to 2^(amount of bits used in the bitset) minus 1 (since a full 1111 bitset is 15, not 16)
 
-	for(int columns=0;columns<maze.size();++columns) {
-		for(int row=0;row<maze.at(columns).size();++row) {
+	for(int columns=0;columns<mazeWalls.size();++columns) {
+		for(int row=0;row<mazeWalls.at(columns).size();++row) {
+
+			mazeWalls.at(columns).at(row)=distribution(rng);
 
 			std::bitset<4> unvisitedNeighbors;
-
 			try {
 				if(!visitedCells.at(columns).at(row-1))
 					unvisitedNeighbors.set(wall::north);
@@ -36,7 +43,17 @@ int main() {
 			} catch (std::out_of_range const& exception) {
 				//std::clog<<"Cell neighbor was beyond edge of unvisitedNeighbors (west): "<<exception.what()<<'\n';
 			}
+
+			std::cout<<mazeWalls.at(columns).at(row)<<" ";
+
+			//if(unvisitedNeighbors.any()) {
+			//	wall nextCell;
+			//	do {
+			//		nextCell=std::uniform_int_distribution<std::mt19937::result_type> distribution(0,unvisitedNeighbors.size());
+			//	} while(!unvisitedNeighbors.test(distribution));
+			//}
 		}
+		std::cout<<'\n';
 	}
 
 	return 0;
